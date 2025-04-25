078a975350e392092345452b9e02101247e2b8fb
Shannon <xXSHANNONTXx@hotmail.com>
2025-04-25 17:44:25 +0100

    added routing for posting a new task

--- tasky-api/api/tasks/tasksData.js
+++ tasky-api/api/tasks/tasksData.js
@@ -1,8 +1,10 @@
 export const tasksData = {
-    page: 1,
-    total_pages: 1,
-    total_results: 3,
-    tasks: [{ id: "1", title: "Dishes", description: "Empty dishwasher", deadline: "Today", done: false, priority: "low" },
-    { id: "2", title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false, priority: "medium" },
-    { id: "3", title: "Tidy up", deadline: "Today", done: false, priority: "high" }]
+
+    "id": "67f9d9ef-cd5b-4adc-99ab-edbcd152dde9",
+    "title": "Complete Assignment 1",
+    "description":"Finish assignment 1 for WAD2",
+    "deadline":"tomorrow",
+    "priority":"medium",
+    "done":false
+
 };
